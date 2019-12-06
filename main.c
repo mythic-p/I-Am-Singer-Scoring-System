0e0be7f049fc5b7887cbe8d6cded9bb3256f6ff0
Umbrella2iw <umbrella12450@gmail.com>
2019-12-06 10:26:07 +0800

    Update

--- main.c
+++ main.c
@@ -3,6 +3,22 @@
 #define WIDTH 80
 #define HEIGHT 20
 
+struct Sing
+{
+	char name[10];
+	double judgerScoreAvg;
+	double audienceScoreAvg;
+};
+typedef struct Sing Sing;
+
+struct Singer
+{
+	char name[5];
+	Sing* sings;
+	int singAmount;
+};
+typedef struct Singer Singer;
+
 void initSystem();
 
 int main()
